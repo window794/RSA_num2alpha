1f7d82a8bd39f423a6075b59057b0095c0c3f1a1
broco <broco@HP-SPECTRE>
2021-12-21 13:44:38 +0900

    プロジェクト ファイルを追加します。

---
+++ RSA/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RSA
+{
+    class Program //クラスの定義
+    {
+        public String Num2alpha(int num)
+        {
+            String words = ""; //初期値を設定
+            String splnums = "";
+            List<string> nums = new List<string>(); //動的配列の宣言
+            int len = num.ToString().Length;
+            if (len % 2 == 0) //文字列の長さが偶数の場合のみ実行する
+            {
+                for (int i = 0; i < len - 1; i += 2)
+                {
+                    splnums = num.ToString().Substring(i, 2); //numを二文字ずつに分割
+                    nums.Add(splnums);
+                }
+
+                foreach (var index in nums)
+                {
+                    words += Convert.ToChar(Int32.Parse(index) + 64);
+                    //65がアルファベットAの始まる十進数上での文字コード
+                    //出てきた数字＋65 -1→出てきた数字＋64して、文字コードを算出
+                    //それをToCharで変換
+                }
+                return words;
+            }
+            else
+            {
+                words = "偶数桁じゃないので変換されませんでした";
+            }
+            return words;
+        }
+        static void Main(string[] args)
+        {
+            int p = 37;
+            int q = 71;
+            int e = 79;
+            int c = 904;
+            int m = 0; //何かを初期化しないで宣言しちゃだめだよ
+            int n = p * q;
+
+            for (int i = 0; i < e; i++)
+            {
+                if (i * (p - 1) * (q - 1) % e == e - 1)
+                {
+                    m = i;
+                    //Console.WriteLine(m);
+                    break;
+                }
+            }
+            int d = (m * (p - 1) * (q - 1) + 1) / e;
+            System.Numerics.BigInteger M = System.Numerics.BigInteger.Pow(c, d) % n;
+            Program obj = new Program();
+            String s2 = obj.Num2alpha((int)M); //メソッド実行
+            String s = String.Format("\r\nm={0}\r\nd={1}\r\nM={2}\r\nアルファベット変換={3}", m, d, M, s2);
+            Console.WriteLine(s);
+        }
+    }
+}
